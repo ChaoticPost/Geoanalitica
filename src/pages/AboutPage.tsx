8e3c70ec494ce138e9bbd9edd0aa318c39648d09
Daria Chugunova <dariachugunova2003@gmail.com>
2025-07-22 23:27:47 +0300

    add: добавлена страница About

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import { AboutSection } from '@/components/sections/AboutSection'
+
+const AboutPage = () => {
+    return (
+        <div className="min-h-screen">
+            <AboutSection />
+        </div>
+    )
+}
+
+export default AboutPage 
