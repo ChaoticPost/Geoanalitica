fbb78a8bc615c7462d14a407d1a018724676b87d
Daria Chugunova <dariachugunova2003@gmail.com>
2025-07-23 09:09:50 +0300

    refactor: обновлена страница Contact для использования новой секции

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,11 +1,11 @@
 import { ContactSection } from '@/components/sections/ContactSection'
 
 const ContactPage = () => {
-    return (
-        <div className="min-h-screen">
-            <ContactSection />
-        </div>
-    )
+  return (
+    <div className="min-h-screen">
+      <ContactSection />
+    </div>
+  )
 }
 
 export default ContactPage 
