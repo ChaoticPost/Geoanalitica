3a23b2d3faeccd2a7013f6b97ee842ee74376a51
Daria Chugunova <dariachugunova2003@gmail.com>
2025-07-26 23:10:49 +0300

    refactor(models): обновлены поля geo

--- backend/models/geo.py
+++ backend/models/geo.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from geoalchemy2 import Geometry
-from app.db.base import TimestampedBase
+from db.base import TimestampedBase
 
 
 class GeoPoint(TimestampedBase):
     __tablename__ = "geo_points"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, nullable=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(1000))
     geometry = Column(Geometry(geometry_type="POINT", srid=4326))
     user_id = Column(Integer, ForeignKey("users.id"))
 
@@ -17,7 +17,7 @@ class GeoPolygon(TimestampedBase):
     __tablename__ = "geo_polygons"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, nullable=True)
+    name = Column(String(255), nullable=False)
+    description = Column(String(1000))
     geometry = Column(Geometry(geometry_type="POLYGON", srid=4326))
     user_id = Column(Integer, ForeignKey("users.id"))
