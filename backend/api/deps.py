e0857ca12a679a17eb7257c1668a18aa5445c755
Daria Chugunova <dariachugunova2003@gmail.com>
2025-07-26 23:08:57 +0300

    fix(api): обновлены зависимости в deps.py

--- backend/api/deps.py
+++ backend/api/deps.py
@@ -1,42 +1,42 @@
 from typing import Generator, Optional
+
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
+from jose import jwt
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.config import settings
-from app.core.security import verify_password
-from app.db.session import get_db
-from app.models.user import User
-from app.schemas.user import TokenPayload
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
+from core import security
+from core.config import settings
+from db.session import get_db
+from models.user import User
+from schemas.user import TokenPayload
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
 
 
 async def get_current_user(
-    db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)
+    db: AsyncSession = Depends(get_db), token: str = Depends(reusable_oauth2)
 ) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
         token_data = TokenPayload(**payload)
-        if token_data.sub is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-
-    user = await db.get(User, token_data.sub)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = await db.query(User).filter(User.id == token_data.sub).first()
     if not user:
-        raise credentials_exception
+        raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
-async def get_current_active_user(
+def get_current_active_user(
     current_user: User = Depends(get_current_user),
 ) -> User:
     if not current_user.is_active:
@@ -44,7 +44,7 @@ async def get_current_active_user(
     return current_user
 
 
-async def get_current_active_superuser(
+def get_current_active_superuser(
     current_user: User = Depends(get_current_user),
 ) -> User:
     if not current_user.is_superuser:
