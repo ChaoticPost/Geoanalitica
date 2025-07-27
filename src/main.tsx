e3aef78ba8026071cadb2efbd05b85f70a4754bc
Daria Chugunova <dariachugunova2003@gmail.com>
2025-07-28 01:09:46 +0300

    feat(core): обновлены провайдеры приложения

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './providers/ThemeProvider';
-import App from './App';
+import { App } from './App';
 
 // Импортируем стили
 import './styles/index.css';
